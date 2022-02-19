16de0427f94223d7ac6aa45dd48512adc287ba26
giannisk <i.koutivas@gmail.com>
2022-02-19 19:52:04 +0200

    without face numbers

--- detectfaces2.py
+++ detectfaces2.py
@@ -16,7 +16,6 @@ with open('links.txt', 'r') as f:
 
 def detect_faces_uri(uri):
     global results
-    global faces_number
     """Detects faces in the file located in Google Cloud Storage or the web."""
     from google.cloud import vision
     client = vision.ImageAnnotatorClient()
@@ -31,30 +30,25 @@ def detect_faces_uri(uri):
     # Names of likelihood from google.cloud.vision.enums
     likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
                        'LIKELY', 'VERY_LIKELY')
-    print('Faces:' + str(len(faces)))
-    faces_number = str(len(faces))
+    print('Faces:')
     results = []
-    for index3, face in enumerate(faces):
+    for face in faces:
         print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
         print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
         print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
         print('Sorrow: {}'.format(likelihood_name[face.sorrow_likelihood]))
 
-        results.append([])
         
-        results[index3] = ['anger: {}'.format(likelihood_name[face.anger_likelihood]),
+        results = ['anger: {}'.format(likelihood_name[face.anger_likelihood]),
         'joy: {}'.format(likelihood_name[face.joy_likelihood]),
         'surprise: {}'.format(likelihood_name[face.surprise_likelihood]),
         'Sorrow: {}'.format(likelihood_name[face.sorrow_likelihood])
         ]
-        
 
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in face.bounding_poly.vertices])
 
         print('face bounds: {}'.format(','.join(vertices)))
-    
-    print(len(results), " ", list(results))
 
     if response.error.message:
         raise Exception(
@@ -77,12 +71,10 @@ for index, name in enumerate(myNames):
 
         img = Image.open('./images/image'+str(index+1)+'.jpg')
         I1 = ImageDraw.Draw(img)
-        resultsfile.write(name + "," + faces_number + ",")
-        I1.text((0, 0), str(faces_number), fill=(255, 0, 0), font=font)
+        resultsfile.write(name + ",")
         for index2, result in enumerate(results):
-            for index4, innerresult in enumerate(result):
-                I1.text((0, 30 + index4 * 30 + index2 * 130), str(innerresult), fill=(255, 0, 0), font=font)
-                resultsfile.write(str(result) + ", ")
+            I1.text((0, 0 + index2 * 30), str(result), fill=(255, 0, 0), font=font)
+            resultsfile.write(str(result) + ", ")
         resultsfile.write("\n")
         img.save('./images/image'+str(index+1)+'.jpg')
 
