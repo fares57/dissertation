c7b48ba16056ce946a078f6e9b31eb0de1f6bf91
giannisk <i.koutivas@gmail.com>
2022-02-20 14:03:43 +0200

    detect text ready

--- detecttext.py
+++ detecttext.py
@@ -1,5 +1,13 @@
 from asyncio.windows_events import NULL
 import webbrowser
+import os
+import sys
+import requests
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import urllib.request
+
 
 with open('links.txt', 'r') as f:
     myNames = f.readlines()
@@ -7,6 +15,7 @@ with open('links.txt', 'r') as f:
 def detect_text_uri(uri):
     """Detects text in the file located in Google Cloud Storage or on the Web.
     """
+    global results
     from google.cloud import vision
     client = vision.ImageAnnotatorClient()
     image = vision.Image()
@@ -15,15 +24,19 @@ def detect_text_uri(uri):
     response = client.text_detection(image=image)
     texts = response.text_annotations
     print('Texts:')
+    results = []
+    if texts == []:
+        print('no texts')
+        results = ['notext']
+    for index2, text in enumerate(texts):
+        # print(text.description)
+        results.append([])
+        results[index2] = text.description
+        # vertices = (['({},{})'.format(vertex.x, vertex.y)
+        #             for vertex in text.bounding_poly.vertices])
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
-
+        #print('bounds: {}'.format(','.join(vertices)))
+    print(list(results))
     if response.error.message:
         raise Exception(
             '{}\nFor more info on error messages, check: '
@@ -32,10 +45,19 @@ def detect_text_uri(uri):
 
 for index, name in enumerate(myNames):
     print('iteration: ', index + 1, ' of: ', len(myNames))
+    resultsfile = open('./textresults.xls', 'a')
     try:
         detect_text_uri(str(name))
-        webbrowser.open(str(name))
         print('\n')
+        # resultsfile.write(str(name))
+        resultsfile.write(str(name) + '*' + results[0].replace('\n', ' ') + '\n')
+        # resultsfile.write("\n")
+        # resultsfile.writelines("")
+        # for index3, result in enumerate(results):
+        #     resultsfile.write(str(result))
+        # resultsfile.write("\n")
     except:
         print('Not Accessible', '\n')
+        resultsfile.write(str(name) + '*' + results[0].replace('\n', ' ') + '\n')
         pass
+    resultsfile.close()
