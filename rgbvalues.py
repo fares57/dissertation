d4100f5eecc4c7d73d93cf8a832596324ba2f666
giannisk <i.koutivas@gmail.com>
2022-03-01 19:51:50 +0200

    03012022 evening

--- rgbvalues.py
+++ rgbvalues.py
@@ -6,7 +6,7 @@ from yachalk import chalk
 
 
 global chosen_color
-chosen_color = "#03090a"
+chosen_color = "#1e2db0"
 
 
 def get_rgb(color):
@@ -19,6 +19,8 @@ def get_rgb(color):
     global blue 
     blue = 0
     blue = ImageColor.getcolor(color, "RGB")[2]
+
+    print(red, green, blue)
 
     global same
     if ((abs(abs(red) - abs(green))) <=20) and ((abs(abs(red) - abs(blue))) <=20) and ((abs(abs(green) - abs(blue))) <=20):
@@ -35,6 +37,14 @@ def get_rgb(color):
     elif ((red > 220) and (green > 220) and (blue > 220)) and (same == 1):
         # print("white")
         print(chalk.bg_hex(chosen_color).black("white"))
+    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (red > green) and (red > blue):
+        print(chalk.bg_hex(chosen_color).black("red"))
+    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (green > red) and (green > blue):
+        print(chalk.bg_hex(chosen_color).black("green"))
+    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (blue > red) and (blue > green):
+        print(chalk.bg_hex(chosen_color).white("blue"))
+    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (blue > red) and (blue > green):
+        print(chalk.bg_hex(chosen_color).white("blue"))    
     else:
         print("not grey")
         print(chalk.bg_hex(chosen_color).black("not grey"))
