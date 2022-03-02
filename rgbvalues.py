e4e6bb7cd5a937af665408c5c7512809045d5fdb
giannisk <i.koutivas@gmail.com>
2022-03-02 21:20:25 +0200

    01022022

--- rgbvalues.py
+++ rgbvalues.py
@@ -3,11 +3,40 @@ from genericpath import samefile
 from pickle import TRUE
 from PIL import ImageColor
 from yachalk import chalk
+import os
+
+with open('colors.txt', 'r') as f:
+    colors = f.readlines()
+
+
+def sames(red,green,blue):
+    if ((abs(abs(red) - abs(green))) <=20) and ((abs(abs(red) - abs(blue))) <=20) and ((abs(abs(green) - abs(blue))) <=20):
+        return True
+    else:
+        return False
+
+def triad_difference(first, second):
+    if (abs(first - second)) >=50:
+        return True
+    else:
+        return False
+
+def far_from_borders(red, green, blue):
+    if ((red >= 30) or (green >= 30) or (blue >= 30)) and ((red <= 220) or (green <= 220) or (blue <= 220)):
+        return True
+    else:
+        return False
+
+def bigger(first, second):
+    if first > second:
+        return True
+    else:
+        return False
 
 
 global chosen_color
-chosen_color = "#1e2db0"
-
+# chosen_color = "#fff200"
+chosen_color = ""
 
 def get_rgb(color):
     global red 
@@ -21,37 +50,45 @@ def get_rgb(color):
     blue = ImageColor.getcolor(color, "RGB")[2]
 
     print(red, green, blue)
+    print(sames(red,green,blue))
+    print("Red and Green are different by 50:",triad_difference(red,green))
+    print("is there at least a color far from borders: ", far_from_borders(red,green,blue))
 
-    global same
-    if ((abs(abs(red) - abs(green))) <=20) and ((abs(abs(red) - abs(blue))) <=20) and ((abs(abs(green) - abs(blue))) <=20):
-        same = 1
-    else:
-        same = 0
-
-    if ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (same == 1):
+    if far_from_borders(red, green, blue) and (sames(red,green,blue)):
         # print("grey")
         print(chalk.bg_hex(chosen_color).black("grey"))
-    elif ((red < 30) and (green < 30) and (blue < 30)) and (same == 1):
+    elif ((red < 30) and (green < 30) and (blue < 30)) and (sames(red,green,blue)):
         # print("black")
         print(chalk.bg_hex(chosen_color).black("black"))
-    elif ((red > 220) and (green > 220) and (blue > 220)) and (same == 1):
+    elif ((red > 220) and (green > 220) and (blue > 220)) and (sames(red,green,blue)):
         # print("white")
         print(chalk.bg_hex(chosen_color).black("white"))
-    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (red > green) and (red > blue):
+    elif far_from_borders(red, green, blue) and (bigger(red, green)) and (bigger(red, blue)) and (triad_difference(red,green)):
         print(chalk.bg_hex(chosen_color).black("red"))
-    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (green > red) and (green > blue):
+    elif far_from_borders(red, green, blue) and (bigger(green, red)) and (bigger(green, blue)) and (triad_difference(blue, green)):
         print(chalk.bg_hex(chosen_color).black("green"))
-    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (blue > red) and (blue > green):
-        print(chalk.bg_hex(chosen_color).white("blue"))
-    elif ((red >= 30) and (green >= 30) and (blue >= 30)) and ((red <= 220) and (green <= 220) and (blue <= 220)) and (blue > red) and (blue > green):
-        print(chalk.bg_hex(chosen_color).white("blue"))    
+    elif far_from_borders(red, green, blue) and (bigger(blue, red)) and (bigger(blue, green)) and (triad_difference(blue, red)) and (triad_difference(blue, green)):
+        print(chalk.bg_hex(chosen_color).black("blue"))
+    elif far_from_borders(red, green, blue) and (not triad_difference(red, green)) and (bigger(red, blue) or (bigger(green, blue))) and not triad_difference(blue, red) and not triad_difference(blue, green):
+        print(chalk.bg_hex(chosen_color).black("yellow"))
+    elif far_from_borders(red, green, blue) and (not triad_difference(blue, green)) and (bigger(blue, red) or (bigger(green, red))):
+        print(chalk.bg_hex(chosen_color).black("cyan"))-
+    elif far_from_borders(red, green, blue) and (not triad_difference(blue, red)) and ((bigger(blue, green)) or (bigger(red, green))):
+        print(chalk.bg_hex(chosen_color).black("magenta"))
     else:
         print("not grey")
-        print(chalk.bg_hex(chosen_color).black("not grey"))
+        print(chalk.bg_hex(chosen_color).black("other"))
 
 
 
-get_rgb(chosen_color)
+for color in colors:
+    chosen_color = "#"+color.split('\n')[0]
+    try:
+        get_rgb(chosen_color)
+    except:
+        # print('wrong color code')
+        continue
+
 
 
 
